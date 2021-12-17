fd72beb40a58cd996c737d31768bafd6ad3a0903
yousioph25 <Yousioph25@gmail.com>
2021-12-17 12:31:54 +0100

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,52 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const RecipeApp());
+}
+
+class RecipeApp extends StatelessWidget {
+  const RecipeApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+// 1
+  @override
+  Widget build(BuildContext context) {
+    // 2
+    final ThemeData theme = ThemeData();
+    // 3
+    return MaterialApp(
+      // 4
+      title: 'Recipe Calculator',
+      // 5
+      theme: theme.copyWith(
+        colorScheme: theme.colorScheme.copyWith(
+          primary: Colors.grey,
+          secondary: Colors.black,
+        ),
+      ),
+      // 6
+      home: const MyHomePage(title: 'Recipe Calculator'),
+    );
+
+  }
+}
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    // 1
+    return Scaffold(
+      // 2
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      // 3
+      body: SafeArea(
+        // TODO: Replace child: Container()
+        // 4
+        child: Container(),
+      ),
+    );
+  }
+
+// TODO: Add buildRecipeCard() here
+}
